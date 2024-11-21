849335a68794d9816ae040b47bac6b5d059a326a
kamisetty sudheer <kamisettysudheer123@gmail.com>
2024-11-21 11:28:31 +0530

    Update sudheerFindAPath.js

--- sudheerFindAPath.js
+++ sudheerFindAPath.js
@@ -1,1 +1,28 @@
 // find a path in mine field...
+/*
+create a path
+create only one start and one end
+other than the path in a land , all are mines
+if 
+*/
+
+// make a boxes as dynamic   🟦 ⬜ 🟥
+
+const blueBox = "🟦";
+
+function getBoxes(noOfBoxes, row) {
+  if (noOfBoxes < 0) {
+    return 0;
+  }
+
+  if (row.length === 10) {
+    console.log(row);
+    row = "";
+  }
+
+  row = row + blueBox;
+
+  return getBoxes(noOfBoxes - 1, row);
+}
+
+getBoxes(25, "");
