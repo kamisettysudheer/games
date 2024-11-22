a2191cb344b0e440bd655ac2226219e6252d2248
kamisetty sudheer <kamisettysudheer123@gmail.com>
2024-11-22 17:46:27 +0530

    Update FindAPath.js

--- FindAPath.js
+++ FindAPath.js
@@ -9,11 +9,43 @@ other than the path in a land , all are mines
 const blueBox = "🟦";
 let rowPos = 0;
 let columnPos = 0;
+const LEFT = 0;
+const RIGHT = 1;
+const UP = 2;
+const DOWN = 3;
+
+function delay() {
+  for (let i = 0; i < 1000000000; i++) { }
+}
+
+function isPathCorrect() {
+  const path = "00-01-11-12-22-32-33-43-44";
+  for (let i = 0; i < path.length; i += 3) {
+    if ((+path[i]) === rowPos && (+path[i + 1]) === columnPos) {
+      return true;
+    }
+  }
+
+
+  return false;
+}
+
+function getDirection(direction) {
+  switch (direction) {
+    case "a": return LEFT;
+    case "d": return RIGHT;
+    case "w": return UP;
+    case "s": return DOWN;
+  }
+
+  return -1;
+}
 
 function movement(direction) {
   // 0 = left; 1 = right; 2 = up; 3 = down;
+  direction = getDirection(direction);
 
-  if (direction === 0 || direction === 1) {
+  if (direction === LEFT || direction === RIGHT) {
     let offSetCol = (direction - 1) ** (direction - 1);
 
     if (columnPos + offSetCol < 0 || columnPos + offSetCol > 4) {
@@ -23,7 +55,7 @@ function movement(direction) {
     columnPos = columnPos + offSetCol;
   }
 
-  if (direction === 2 || direction === 3) {
+  if (direction === UP || direction === DOWN) {
     let offSetRow = (direction - 3) ** (direction - 3);
 
     if (rowPos + offSetRow < 0 || rowPos + offSetRow > 4) {
@@ -40,7 +72,7 @@ function getBoxes(noOfRows, noOfColoumns) {
   for (let row = 0; row < noOfRows; row++) {
     for (let coloumn = 0; coloumn < noOfColoumns; coloumn++) {
       if (row === rowPos && coloumn === columnPos) {
-        boxes = boxes + "🧑‍💼";
+        boxes = boxes + (isPathCorrect() ? "🧑‍💼" : "💣");
       } else {
         boxes = boxes + blueBox;
       }
@@ -52,7 +84,15 @@ function getBoxes(noOfRows, noOfColoumns) {
   return boxes + "\t  ↘️";
 }
 
-function game() {
+function takeDirection() {
+  // console.log(" RIGHT -> D \n LEFT  -> A \n UP    -> W \n DOWN  -> S");
+  // "👆👇👉👈"
+
+  console.log("   👆 \t\t   W \n 👈👇👉 \t A S D");
+  return prompt("Enter the direction number : ");
+}
+
+function gameLoop() {
   console.clear();
   console.log(getBoxes(5, 5));
 
@@ -60,12 +100,19 @@ function game() {
     return "\t 🏆🏆🏆 congrats!, you find the path 🏆🏆🏆 \n";
   }
 
-  console.log("0 = left; 1 = right; 2 = up; 3 = down");
-  const direction = +prompt("Enter the direction number : ");
-
+  const direction = takeDirection();
   movement(direction);
 
-  return game();
+  if (!isPathCorrect()) {
+    console.clear();
+    console.log(getBoxes(5, 5));
+    delay();
+    rowPos = 0;
+    columnPos = 0;
+    return gameLoop();
+  }
+
+  return gameLoop();
 }
 
-console.log(game());
+console.log(gameLoop());
